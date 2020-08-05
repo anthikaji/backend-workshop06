d4050d7b5b5f8b497e7ab2bff87c7ad4efa9c333
Anthika Jirattananon <anthikajir@mathregit.group>
2020-08-05 14:38:49 +0700

    step3

---
+++ README.md
@@ -0,0 +1,15 @@
+# workshop-springboot-101
+
+```
+$gradlew
+$gradlew test
+$gradlew bootRun
+
+```
+
+Run in production
+```
+$gradlew bootJar
+$java -jar build/libs/demo-0.0.1-SNAPSHOT.jar 
+```
+Open url=http://localhost:8080/ in a web browser
---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users")
+    public PagingResponse getAllUser(
+           @RequestParam(defaultValue = "1") int page,
+           @RequestParam(name = "item_per_page" ,defaultValue = "10") int itemPerPage) {
+
+        PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
+        List<UsersResponse> usersResponseList = new ArrayList<>();
+        usersResponseList.add(new UsersResponse(1, "User 1"));
+        usersResponseList.add(new UsersResponse(2, "User 2"));
+        usersResponseList.add(new UsersResponse(3, "User 3"));
+        pagingResponse.setUsersResponse(usersResponseList);
+        return pagingResponse;
+    }
+
+    @GetMapping("/users/{id}")
+    public UsersResponse getUserById(@PathVariable int id) {
+
+        return new UsersResponse(id, "User " + id);
+    }
+    @PostMapping("/users")
+    public UsersResponse createNewUser(@RequestBody NewUserRequest request){
+        return  new UsersResponse(0,request.getName() + request.getAge());
+    }
+}
+
