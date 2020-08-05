18f3b2d8c69788f0c4072393d9bdab60a80518d6
Anthika Jirattananon <anthikajir@mathregit.group>
2020-08-05 13:52:30 +0700

    step2

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -2,16 +2,31 @@ package com.example.demo;
 
 import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
-    @GetMapping ("/users")
-    public UserResponse[] getAllUser(){
-        UserResponse[] userResponses = new UserResponse[2];
-        userResponses[0] = new UserResponse(1, "User1");
-        userResponses[1] = new UserResponse(2, "User2");
-        return  userResponses;
+    public PagingResponse getAllUser(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(name = "item_per_page", defaultValue = "10") int itemPerPage) {
+
+        PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
+        List<UserResponse> usersResponseList = new ArrayList<>();
+        usersResponseList.add(new UserResponse(1, "User 1"));
+        usersResponseList.add(new UserResponse(2, "User 2"));
+        usersResponseList.add(new UserResponse(3, "User 3"));
+        pagingResponse.setUsersResponse(usersResponseList);
+        return pagingResponse;
+    }
+
+    @GetMapping("/users/{id}")
+    public UserResponse getUserById(@PathVariable int id) {
+        return new UserResponse(id, "User " + id);
     }
 }
